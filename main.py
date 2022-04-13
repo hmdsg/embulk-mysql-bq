4500611a33bb8cafe51efedb60d0c89ab73fcc7f
shogohamda <>
2022-04-13 12:57:49 +0900

    wip

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def return_main():
+    print("start exec")
+    result = exec_embulk()
+    print("end exec")
+
+    return result
+
 def exec_embulk():
     try:
-        print ("embulk start")
         res = subprocess.run("embulk -v", shell=True, check=True, capture_output=True, text=True)
-        print ("embulk end")
         result = res.stdout
-        print ("end stdout")
-        print (res, result)
     except subprocess.CalledProcessError as e:
         print ("error!")
         print(e.cmd)
@@ -25,6 +28,7 @@ def exec_embulk():
         result = e.stdout
 
     return result
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
